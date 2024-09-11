bc6fc48b1c7667ddca00c7cc50991e0129c1bb8c
sagarghorsai <dostisagar14@gmail.com>
2024-09-10 20:07:09 -0400

    Update

--- Assets/_Scripts/Scene/SceneManager.cs
+++ Assets/_Scripts/Scene/SceneManager.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+    public void StartGame()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
+
+    public void Options()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Options");
+
+    }
+
+
 }
+   
