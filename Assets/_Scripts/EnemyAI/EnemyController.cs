951cc1f99d6217682d85217173d408c3024f9603
Austin Horn <ahornster@gmail.com>
2024-09-16 19:29:45 -0400

    9/10 to 9/13 - AustinHorn
    Created:
    WaypointMovement script,
    PanicMeter Script,
    EnemyController Script (empty)
    Enemyplaceholder prefab
    EnemyWaypoint prefab
    TestScene_Austin scene

---
+++ Assets/_Scripts/EnemyAI/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*PhantomRealm Studio - Life of a Recluse
+ * Austin Horn
+ * CSCI 448, Davenport University
+ * Instructor: David Kroggman
+ * 
+ * Script: EnemyController
+ * Summary: A script for universal items for each enemy.
+ *          Including: 
+ *                 + Change Perspectives based on direction the enemy is moving
+ *                 + 
+ */
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
