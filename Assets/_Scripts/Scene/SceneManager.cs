f0ec4f91a99b516aa1125c81c65077b61cda2680
sagarghorsai <dostisagar14@gmail.com>
2024-09-28 23:12:31 -0400

    Last Alpha Build Update

--- Assets/_Scripts/Scene/SceneManager.cs
+++ Assets/_Scripts/Scene/SceneManager.cs
@@ -13,11 +13,6 @@ public class SceneManager : MonoBehaviour
     {
         Application.Quit();
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit;
-#endif
     }
 
     public void Options()
