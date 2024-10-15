0333752cb4b489f5f3c0effabb4ba20d347c7639
Austin Horn <ahornster@gmail.com>
2024-10-15 19:09:34 -0400

    Week 10/15
    Randomized movement script
    enemy prefabs

--- Assets/_Scripts/EnemyAI/HuntPlayerMovement.cs
+++ Assets/_Scripts/EnemyAI/HuntPlayerMovement.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*PhantomRealm Studio - Life of a Recluse
+ * Austin Horn
+ * CSCI 448, Davenport University
+ * Instructor: David Kroggman
+ * 
+ * Title: HuntPlayerMovement
+ * Summary: A movement script intended for the Manager Enemy in "Life of a Recluse"
+ *          Script should identify current location of player, move towards player and try to avoid enviromental objects (shelves, displays, etc.)
+ *          
+ */
+
 public class HuntPlayerMovement : EnemyController
 {
 
-    private void Awake()
+    void Start()
     {
         
     }
